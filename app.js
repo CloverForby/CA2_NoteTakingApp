d40c203d85b80cebc1994def66abf73365cec35f
CloverForby <rustypennyten@gmail.com>
2025-07-24 21:44:07 +0800

    convert to routers - mel

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const mysql = require('mysql2');
 
 const session = require('express-session');
 const flash = require('connect-flash');
+const userRoutes = require('./routes/user');
+const searchRoutes = require('./routes/searchncat');
+const notesRoutes = require('./routes/notes');
 const app = express();
 const connection = mysql.createConnection({
     host:'localhost', //Guys PLs change this to your MySQL host
@@ -32,22 +35,12 @@ app.use(flash());
 app.set('view engine', 'ejs');
 
 
-
-//middleware function on app.js
-const validateRegistration = (req, res, next) => {
-    const {username, email, password} = req.body
-
-    if (!username || !email || !password) {
-        return res.status(400).send('All fields are required.');
-    }
-
-    if (password.length < 6) {
-        req.flash('error', 'Password should be at least 6 or more characters long');
-        req.flash('formData', req.body);
-        return res.redirect('/register')
-    }
-    next();
-};
+//USER ROUTES
+app.use('/',userRoutes);
+//NOTES ROUTES
+app.use('/',notesRoutes);
+//SEARCH ROUTES
+app.use('/',searchRoutes);
 
 //Define routes
 app.get('/',(req,res)=>{
@@ -63,77 +56,6 @@ app.get('/',(req,res)=>{
     });
 
 });
-
-app.get('/deleteNote/:id',(req,res)=> {
-    const noteId = req.params.id
-    const sql = "DELETE FROM notes WHERE noteId = ?";
-    connection.query(sql, [noteId], (error, results) => {
-        if (error) {
-            console.error("Error deleting Note:", error);
-        } else {
-            res.redirect ('/')
-        }
-    })
-})
-//Login page route on app.js
-app.get('/register', (req, res) => {
-    res.render('register', { messages: req.flash('error'), formData: req.flash('formData')[0] });
-});
-
-//Register route on app.js
-app.post ('/register', validateRegistration, (req, res) => {
-    const { username, email, password} = req.body;
-    const sql = 'INSERT INTO users (username, email, password) VALUES (?, ?, SHA1(?))';
-    connection.query(sql, [username, email, password], (err, result) => {
-        if (err) {
-            throw err;
-        }
-        console.log(result);
-        req.flash('success', 'Registration successful! Please log in.');
-        res.redirect('/login');
-    });
-});
-
-//Login page route on app.js
-app.get('/login', (req, res) => {
-    res.render('login', {
-        messages: req.flash('success'),
-        errors: req.flash('error')
-    });
-});
-
-//login route for form submission on app.js
-app.post('/login', (req, res) => {
-    const {email, password} = req.body;
-
-    if (!email || !password) {
-        req.flash('error', 'All fields are required.');
-        return res.redirect('/login');
-    }
-
-    const sql = 'SELECT * FROM users WHERE email = ? AND password = SHA1(?)'
-    connection.query(sql, [email, password], (err, results) => {
-        if (err) {
-            throw err;
-        }
-
-        if (results.length > 0) {
-            req.session.user = results[0];
-            req.flash('success', 'Login successful!');
-            res.redirect('/dashboard');
-        } else {
-            req.flash('error', 'Invalid email or password.');
-            res.redirect('/login');
-        }
-    });
-});
-
-//logout route on app.js
-app.get('/logout', (req, res) => {
-    req.session.destroy();
-    res.redirect('/');
-});
-
 
 //Set up view engine
 app.set('view engine','ejs');
---
+++ routes/user.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const router = express.Router();
+
+
+//CODE BY IRFAN + minor fixes by melissa
+
+
+//middleware function on app.js
+const validateRegistration = (req, res, next) => {
+    const {username, email, password} = req.body
+
+    if (!username || !email || !password) {
+        return res.status(400).send('All fields are required.');
+    }
+
+    if (password.length < 6) {
+        req.flash('error', 'Password should be at least 6 or more characters long');
+        req.flash('formData', req.body);
+        return res.redirect('/register')
+    }
+    next();
+};
+
+//Register page load
+router.get('/register', (req, res) => {
+    res.render('register', { messages: req.flash('error'), formData: req.flash('formData')[0] });
+});
+
+//Register post
+router.post ('/register', validateRegistration, (req, res) => {
+    const { username, email, password} = req.body;
+    const sql = 'INSERT INTO users (username, email, password) VALUES (?, ?, SHA1(?))';
+    connection.query(sql, [username, email, password], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        console.log(result);
+        req.flash('success', 'Registration successful! Please log in.');
+        res.redirect('/login');
+    });
+});
+
+//Login page load
+router.get('/login', (req, res) => {
+    res.render('login', {
+        messages: req.flash('success'),
+        errors: req.flash('error')
+    });
+});
+
+//login post
+router.post('/login', (req, res) => {
+    const {email, password} = req.body;
+
+    if (!email || !password) {
+        req.flash('error', 'All fields are required.');
+        return res.redirect('/login');
+    }
+
+    const sql = 'SELECT * FROM users WHERE email = ? AND password = SHA1(?)'
+    connection.query(sql, [email, password], (err, results) => {
+        if (err) {
+            throw err;
+        }
+
+        if (results.length > 0) {
+            req.session.user = results[0];
+            req.flash('success', 'Login successful!');
+            res.redirect('/dashboard');
+        } else {
+            req.flash('error', 'Invalid email or password.');
+            res.redirect('/login');
+        }
+    });
+});
+
+//logout route on app.js
+router.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+
+module.exports = router;
